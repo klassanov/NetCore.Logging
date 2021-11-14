d17396860fed1fe9f52c532568ac5915959206f8
Alexander Klassanov <klassanov@gmail.com>
2021-11-14 23:06:16 +0200

    Serilog structured logging

--- Logging.Data/BookService.cs
+++ Logging.Data/BookService.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections.Generic;
 using Logging.Interfaces.Data;
 using Logging.Model;
+using Microsoft.Extensions.Logging;
 
 namespace Logging.Data
 {
     public class BookService : IBookService
     {
+        private readonly ILogger<BookService> logger;
+
+        public BookService(ILogger<BookService> logger)
+        {
+            this.logger = logger;
+        }
+
         public IEnumerable<Book> GetBooks()
         {
-            return new List<Book>()
+            this.logger.LogInformation("In the GetBooks method");
+
+            var books = new List<Book>()
             {
                 new Book{Title="Vinetu", Price=110m},
                 new Book{Title="The great indian", Price=99m},
             };
+
+            this.logger.LogInformation("Retrieved books: {@books}", books);
+
+            return books;
         }
     }
 }
--- Logging.Web/Program.cs
+++ Logging.Web/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace Logging.Web
 {
@@ -7,14 +12,43 @@ namespace Logging.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(LogConfiguration)
+            .WriteTo.File(new JsonFormatter(), path: @"C:\temp\logs\books-log.json", shared: true)
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+            }
+
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+               .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+               .UseSerilog();
+
+
+        public static IConfiguration LogConfiguration => new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
     }
 }
